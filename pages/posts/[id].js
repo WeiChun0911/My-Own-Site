f2f5763ef3a1de89a2d5c6d7807bd37e7f7dce4f
willyliu <willyliu@knowhow.fun>
2022-01-14 13:55:23 +0800

    mdx working

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,6 +1,9 @@
 import Head from "next/head";
 import Layout from "../../components/layout";
 import Date from "../../components/date";
+import Image from "next/image";
+import { MDXRemote } from "next-mdx-remote";
+import { serialize } from "next-mdx-remote/serialize";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 import utilStyles from "../../styles/utils.module.css";
 
@@ -14,14 +17,16 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id);
+  const mdxSource = await serialize(postData.content);
   return {
     props: {
       postData,
+      source: mdxSource,
     },
   };
 }
 
-export default function Post({ postData }) {
+export default function Post({ postData, source }) {
   return (
     <Layout>
       <Head>
@@ -31,8 +36,8 @@ export default function Post({ postData }) {
         <h1 className={utilStyles.headingXl}>{postData.title}</h1>
         <div className={utilStyles.lightText}>
           <Date dateString={postData.date} />
+          <MDXRemote {...source} components={{ Image }} />
         </div>
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </article>
     </Layout>
   );
